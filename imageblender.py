b68210b79e3c2bd2772e90060f5fab20f5ef930a
nick <ncyates@users.noreply.github.com>
2017-08-28 00:09:55 -0700

    working version of multiprocessing

--- imageblender.py
+++ imageblender.py
@@ -1,72 +1,110 @@
 import cv2
+from multiprocessing import Process, Queue
 import os
 import math
 from os import listdir
-from random import shuffle
 from Tkinter import Tk
 from tkFileDialog import askdirectory
 import tkMessageBox
+from random import shuffle
+import time
 
 
-Tk().withdraw()
-srcDir = askdirectory()
-dstDir = srcDir + '/' + 'results'
+start_time = time.time()
 
-try:
-    os.mkdir(dstDir)
-except OSError as e:
-    if e.errno == 17:
-        if tkMessageBox.askokcancel('Folder already exists', 'The output folder already exists, proceeding may overwrite the files it contains.'):
-                pass
+def blend(proc,q,fnListSlice,dstDir,zeroPad):
+    remList = []
+    alpha = .5
+    count = 0
+    while len(fnListSlice)>1:
+        aname = fnListSlice.pop()
+        bname = fnListSlice.pop()
+        imga = cv2.imread(aname, 1)
+        imgb = cv2.imread(bname, 1)
+        imga = cv2.resize(imga, (2592, 1944))
+        imgb = cv2.resize(imgb, (2592, 1944))
+        outName = dstDir + '/' + 'merge_' + proc + '_' + str(count).zfill(zeroPad) + '.jpg'
+        #print('merging ' + aname + ' and ' + bname + ' into ' + outName)
+        imgc = cv2.addWeighted(imga, alpha, imgb, alpha, 0)
+        del (imga)
+        del (imgb)
+        cv2.imwrite(outName, imgc)
+        fnListSlice.insert(0, outName)
+        remList.append(outName)#uncomment to enable file removal
+        del(imgc)
+        count += 1
+    #fileNamesList.append(outName)
+    q.put(outName)
+
+    # To retain all merges, comment out below
+    #for f in remList:
+        #print(f)
+    #for g in fileNamesList:
+        #print(g)
+    #remList.pop()
+    #for r in remList:
+        #os.remove(r)
+
+if __name__ == "__main__":
+
+    Tk().withdraw()
+
+    srcDir = askdirectory()
+    global dstDir
+    dstDir = srcDir + '/' + 'results'
+
+    try:
+        os.mkdir(dstDir)
+    except OSError as e:
+        if e.errno == 17:
+            if tkMessageBox.askokcancel('Folder already exists', 'The output folder already exists, proceeding may overwrite the files it contains.'):
+                    pass
+            else:
+                print('Exiting ImageBlender')
+                sys.exit(1)
+
+
+
+    global fileNamesList
+    fileNamesList = [srcDir + '/' + str(f) for f in listdir(srcDir) if f.endswith('.JPG') or f.endswith('.jpg')]
+    shuffle(fileNamesList)
+    #leading zeros to pad file names for output ordering = ceiling of log base 10 of n+1
+    global zeroPad
+    zeroPad = int(math.ceil(math.log(len(fileNamesList) + 1, 10))) + 1
+    #print(len(fileNamesList)/4)
+
+
+    q = Queue()
+
+    p1 = Process(target = blend, args = ('p1', q, fileNamesList[0:(len(fileNamesList)/4)],dstDir,zeroPad))
+    p2 = Process(target = blend, args = ('p2', q, fileNamesList[len(fileNamesList) / 4:(len(fileNamesList) / 2)-1], dstDir, zeroPad))
+    p3 = Process(target = blend, args = ('p3', q, fileNamesList[len(fileNamesList) / 2:3*(len(fileNamesList) / 4)-1], dstDir, zeroPad))
+    p4 = Process(target = blend, args = ('p4', q, fileNamesList[3*(len(fileNamesList) / 4)-1:len(fileNamesList)], dstDir, zeroPad))
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+
+    count = 1
+    while q.qsize() > 1:
+        imga = cv2.imread(q.get(), 1)
+        imgb = cv2.imread(q.get(), 1)
+        imga = cv2.resize(imga, (2592, 1944))
+        imgb = cv2.resize(imgb, (2592, 1944))
+        if count == 3:
+            outName = dstDir + '/' + 'FinalMerge.jpg'
         else:
-            print('Exiting ImageBlender')
-            sys.exit(1)
+            outName = dstDir + '/' + 'penultimate_' + str(count) + '.jpg'
+        print('merging into ' + outName)
+        imgc = cv2.addWeighted(imga, .5, imgb, .5, 0)
+        cv2.imwrite(outName, imgc)
+        q.put(outName)
+        count +=1
 
-
-#fileNamesList = [f for f in listdir(srcDir) if f.endswith('.JPG') or f.endswith('.jpg')]
-fileNamesList = [srcDir + '/' + str(f) for f in listdir(srcDir) if f.endswith('.JPG') or f.endswith('.jpg')]
-#leading zeros to pad file names for output ordering = ceiling of log base 10 of n+1
-zeroPad = int(math.ceil(math.log(len(fileNamesList) + 1, 10))) + 1
-
-
-#remList = [] #uncomment to enable file removal
-
-'''
-for f in fileNamesList:
-    #fname = srcDir + '/' + str(f)
-    imgNames.append(srcDir + '/' + str(f))
-    #images.append(cv2.imread(fname, 1))
-#shuffle(imgNames)
-'''
-alpha = .5
-
-count = 0
-while len(fileNamesList)>1:
-    aname = fileNamesList.pop()
-    bname = fileNamesList.pop()
-    imga = cv2.imread(aname,1)
-    imgb = cv2.imread(bname, 1)
-    # RESIZE TO MORRIS RESOLUTION
-    imga = cv2.resize(imga, (2592, 1944))
-    imgb = cv2.resize(imgb, (2592, 1944))
-    #RESIZE to arbitrary desktop resolution (stretch / skew etc.)
-    #imga = cv2.resize(imga, (1920,1080))
-    #imgb = cv2.resize(imgb, (1920,1080))
-    #imga = cv2.resize(imga, (800, 1000))
-    #imgb = cv2.resize(imgb, (800, 1000))
-    outName = dstDir + '/' + 'merge_' + str(count).zfill(zeroPad) + '.jpg'
-    #print ('merging ' + aname + ' and ' + bname + ' into ' + fname)
-    imgc = cv2.addWeighted(imga, alpha, imgb, alpha, 0)
-    del(imga)
-    del(imgb)
-
-    cv2.imwrite(outName,imgc)
-    fileNamesList.insert(0, outName)
-    #remList.append(outName)#uncomment to enable file removal
-    #del(imgc)#uncomment to enable file removal
-    count+=1
-
-# To retain all merges, comment out below
-#remList.pop()
-#for in remList:
-#    os.remove(r)
+        print("took %s seconds" % (time.time() - start_time))
