a7022b00f17607d67934e0ee073b26d8a6e2e36e
nick <ncyates@users.noreply.github.com>
2017-08-28 00:40:02 -0700

    fixed division of filenames

--- imageblender.py
+++ imageblender.py
@@ -9,14 +9,15 @@ import tkMessageBox
 from random import shuffle
 import time
 
-
 start_time = time.time()
 
-def blend(proc,q,fnListSlice,dstDir,zeroPad):
+
+def blend(proc, q, fnListSlice, dstDir, zeroPad):
+    print("length is " + str(len(fnListSlice)))
     remList = []
     alpha = .5
     count = 0
-    while len(fnListSlice)>1:
+    while len(fnListSlice) > 1:
         aname = fnListSlice.pop()
         bname = fnListSlice.pop()
         imga = cv2.imread(aname, 1)
@@ -24,26 +25,27 @@ def blend(proc,q,fnListSlice,dstDir,zeroPad):
         imga = cv2.resize(imga, (2592, 1944))
         imgb = cv2.resize(imgb, (2592, 1944))
         outName = dstDir + '/' + 'merge_' + proc + '_' + str(count).zfill(zeroPad) + '.jpg'
-        #print('merging ' + aname + ' and ' + bname + ' into ' + outName)
+        # print('merging ' + aname + ' and ' + bname + ' into ' + outName)
         imgc = cv2.addWeighted(imga, alpha, imgb, alpha, 0)
         del (imga)
         del (imgb)
         cv2.imwrite(outName, imgc)
         fnListSlice.insert(0, outName)
-        remList.append(outName)#uncomment to enable file removal
-        del(imgc)
+        remList.append(outName)  # uncomment to enable file removal
+        del (imgc)
         count += 1
-    #fileNamesList.append(outName)
+    # fileNamesList.append(outName)
     q.put(outName)
 
     # To retain all merges, comment out below
-    #for f in remList:
-        #print(f)
-    #for g in fileNamesList:
-        #print(g)
-    #remList.pop()
-    #for r in remList:
-        #os.remove(r)
+    # for f in remList:
+    # print(f)
+    # for g in fileNamesList:
+    # print(g)
+    # remList.pop()
+    # for r in remList:
+    # os.remove(r)
+
 
 if __name__ == "__main__":
 
@@ -57,29 +59,33 @@ if __name__ == "__main__":
         os.mkdir(dstDir)
     except OSError as e:
         if e.errno == 17:
-            if tkMessageBox.askokcancel('Folder already exists', 'The output folder already exists, proceeding may overwrite the files it contains.'):
-                    pass
+            if tkMessageBox.askokcancel('Folder already exists',
+                                        'The output folder already exists, proceeding may overwrite the files it contains.'):
+                pass
             else:
                 print('Exiting ImageBlender')
                 sys.exit(1)
 
-
-
     global fileNamesList
     fileNamesList = [srcDir + '/' + str(f) for f in listdir(srcDir) if f.endswith('.JPG') or f.endswith('.jpg')]
     shuffle(fileNamesList)
-    #leading zeros to pad file names for output ordering = ceiling of log base 10 of n+1
+    # leading zeros to pad file names for output ordering = ceiling of log base 10 of n+1
     global zeroPad
     zeroPad = int(math.ceil(math.log(len(fileNamesList) + 1, 10))) + 1
-    #print(len(fileNamesList)/4)
+    # print(len(fileNamesList)/4)
 
 
     q = Queue()
 
-    p1 = Process(target = blend, args = ('p1', q, fileNamesList[0:(len(fileNamesList)/4)],dstDir,zeroPad))
-    p2 = Process(target = blend, args = ('p2', q, fileNamesList[len(fileNamesList) / 4:(len(fileNamesList) / 2)-1], dstDir, zeroPad))
-    p3 = Process(target = blend, args = ('p3', q, fileNamesList[len(fileNamesList) / 2:3*(len(fileNamesList) / 4)-1], dstDir, zeroPad))
-    p4 = Process(target = blend, args = ('p4', q, fileNamesList[3*(len(fileNamesList) / 4)-1:len(fileNamesList)], dstDir, zeroPad))
+    n = len(fileNamesList)
+
+    p1 = Process(target=blend, args=('p1', q, fileNamesList[0:(len(fileNamesList) / 4)], dstDir, zeroPad))
+    p2 = Process(target=blend,
+                 args=('p2', q, fileNamesList[(len(fileNamesList) / 4): (len(fileNamesList) / 2)], dstDir, zeroPad))
+    p3 = Process(target=blend,
+                 args=('p3', q, fileNamesList[(len(fileNamesList) / 2): 3 * (len(fileNamesList) / 4)], dstDir, zeroPad))
+    p4 = Process(target=blend,
+                 args=('p4', q, fileNamesList[(3 * (len(fileNamesList) / 4)):len(fileNamesList)], dstDir, zeroPad))
 
     p1.start()
     p2.start()
@@ -105,6 +111,6 @@ if __name__ == "__main__":
         imgc = cv2.addWeighted(imga, .5, imgb, .5, 0)
         cv2.imwrite(outName, imgc)
         q.put(outName)
-        count +=1
+        count += 1
 
         print("took %s seconds" % (time.time() - start_time))
