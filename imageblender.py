2512b42a5f6b7b9a24d6da723810aef1196b3d9b
nick <ncyates@users.noreply.gihub.com>
2017-10-04 11:52:30 -0700

    refactoring

--- imageblender.py
+++ imageblender.py
@@ -11,29 +11,33 @@ import tkFileDialog
 import tkMessageBox
 
 
-def blender(fileNameA, fileNameB, mergeName):
+def blender(fileNameA, fileNameB, blendName, stdDim):
     imageA = cv2.imread(fileNameA, 1)
     imageB = cv2.imread(fileNameB, 1)
-    imageA = cv2.resize(imageA, (2592, 1944))
-    imageB = cv2.resize(imageB, (2592, 1944))
-    imgc = cv2.addWeighted(imageA, .5, imageB, .5, 0)
-    cv2.imwrite(mergeName, imgc)
+    if imageA.shape != stdDim:
+        imageA.cv2.resize(imageA, (stdDim[1], stdDim[0]))
+    if imageB.shape != stdDim:
+        imageB.cv2.resize(imageB, (stdDim[1], stdDim[0]))
+    blend = cv2.addWeighted(imageA, .5, imageB, .5, 0)
+    return cv2.imwrite(blendName, blend)
 
 
-def reducingMerge(process, q, chunk, dstDir, zeroPad):
+def reducer(process, q, chunk, dstDir, zeroPad, stdDim):
     count = 0
     while len(chunk) > 1:
-        a = chunk.pop()
-        b = chunk.pop()
-        outName = dstDir + '/' + 'merge_' + process + '_' + str(count).zfill(zeroPad) + '.jpg'
-        blender(a,b,outName)
-        chunk.insert(0, outName)
+        fileNameA = chunk.pop()
+        fileNameB = chunk.pop()
+        blendName = dstDir + '/' + 'blend_' + process + '_' + str(count).zfill(zeroPad) + '.jpg'
+        if blender(fileNameA,fileNameB,blendName,stdDim):
+            chunk.insert(0, blendName)
+        else:
+            sys.stdout.write('Problem with blending ' + fileNameA + ' & ' + fileNameB)
         count += 1
-    q.put(outName)
+    q.put(chunk[0])
 
 
 if __name__ == "__main__":
-    #start_time = time.time()
+    start_time = time.time()
     Tkinter.Tk().withdraw()
     sourceDir = tkFileDialog.askdirectory()
     destDir = sourceDir + '/' + 'results'
@@ -47,44 +51,49 @@ if __name__ == "__main__":
             else:
                 print('Exiting ImageBlender')
                 sys.exit(1)
-    filenames = [sourceDir + '/' + str(f) for f in os.listdir(sourceDir) if f.endswith('.JPG') or f.endswith('.jpg')]
-    random.shuffle(filenames) # possibly gives better end result blend
-    zeroPad = int(math.ceil(math.log(len(filenames) + 1, 10))) + 1 # leading zeros to pad file names for output ordering = ceiling of log base 10 of n+1
+    fileNames = [sourceDir + '/' + str(f) for f in os.listdir(sourceDir) if f.endswith('.JPG') or f.endswith('.jpg')]
+    sampleImage = cv2.imread(fileNames[0], 1)
+    stdDim = sampleImage.shape  # standard dimensions based on one image. H x W
+    #TODO: what about many different image dimensions? Possibly scan for all and set standard as most common dim.
+    random.shuffle(fileNames)  # possibly gives better end result blend
     reducingQ = multiprocessing.Queue()
     mpProcesses = []
-    numFiles = len(filenames)
+    numFiles = len(fileNames)
     numCores = multiprocessing.cpu_count()
     chunkLength = numFiles / numCores
     chunkStart = 0
     chunkEnd = chunkStart + chunkLength
     partialChunk = numFiles % numCores
     filesChunks = []
+    zeroPad = int(math.ceil(math.log(chunkLength + 1,10))) + 1  # of zeros to pad file names for output ordering
     for i in range(numCores):
-        filesChunks.append(filenames[chunkStart:chunkEnd])
+        filesChunks.append(fileNames[chunkStart:chunkEnd])
         chunkStart = chunkEnd
         chunkEnd = chunkStart + chunkLength
     while partialChunk > 0:
-        filesChunks[len(filesChunks) - 1].append(filenames[(len(filenames)) - partialChunk])
+        filesChunks[len(filesChunks) - 1].append(fileNames[(len(fileNames)) - partialChunk])
         partialChunk -=1
     for i in range(len(filesChunks)):
-        mpProcesses.append(multiprocessing.Process(target=reducingMerge, args=('p' + str(i), reducingQ, filesChunks[i], destDir, zeroPad)))
+        mpProcesses.append(multiprocessing.Process(target=reducer, args=('p' + str(i), reducingQ, filesChunks[i], destDir, zeroPad, stdDim)))
     for i in range(len(mpProcesses)):
         mpProcesses[i].start()
     for i in range(len(mpProcesses)):
         mpProcesses[i].join()
     wrapCount = 1
     while reducingQ.qsize() > 1:
-        a = reducingQ.get()
-        b = reducingQ.get()
+        wrapUpA = reducingQ.get()
+        wrapUpB = reducingQ.get()
         if reducingQ.qsize() == 2:
-            outName = destDir + '/' + 'FinalMerge.jpg'
+            outName = destDir + '/' + '_Final.jpg'
         else:
-            outName = destDir + '/' + 'wrap' + str(wrapCount) + '.jpg'
-        blender(a, b, outName)
-        reducingQ.put(outName)
+            outName = destDir + '/' + '_wrapUp' + str(wrapCount) + '.jpg'
+        if blender(wrapUpA, wrapUpB, outName, stdDim):
+            reducingQ.put(outName)
+        else:
+            print('Problem blending ' + wrapUpA + ' & ' + wrapUpB)
         wrapCount += 1
 
-    for f in os.listdir(destDir): # Comment out to keep all merges on disk
-        if str(os.path.basename(f)) != 'FinalMerge.jpg':
-            os.remove(destDir + '/' + f)
-    #print("took %s seconds" % (time.time() - start_time))
+    # for f in os.listdir(destDir): # Comment out to keep all merges on disk
+    #     if str(os.path.basename(f)) != '_Final.jpg':
+    #         os.remove(destDir + '/' + f)
+    print("took %s seconds" % (time.time() - start_time))
