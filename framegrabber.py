2c37b0e94816bedd3de28ce738552a9c8d2cdf7e
nick <yates.nicholas@gmail.com>
2017-11-06 21:35:41 -0800

    refactor

--- framegrabber.py
+++ framegrabber.py
@@ -1,17 +1,16 @@
 '''
 prototype utility to grab frames from videos and write to images using openCV -- needs cleanup, refactor, remove hard-coding
 '''
-import cv2
+
 import os
 import sys
 import math
 import multiprocessing
-import tkMessageBox
-from os import listdir, mkdir
-from Tkinter import Tk
-from tkFileDialog import askopenfilename, askdirectory
 
-#from timeit import default_timer as timer
+import cv2
+import Tkinter
+import tkMessageBox
+import tkFileDialog
 
 
 def getFrames(filename, outDir, start, end):
@@ -35,17 +34,16 @@ def getFrames(filename, outDir, start, end):
             outFilename = outDir + '/frame_' + str(int(video.get(cv2.CAP_PROP_POS_FRAMES))-1).zfill(6) + '.jpg'
             cv2.imwrite(outFilename, image)
         # exit if Escape is hit
-        if cv2.waitKey(10) == 27:
+        if cv2.waitKey(1) == 27:
             video.release()
             break
 
-
 if __name__ == "__main__":
-    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    Tkinter.Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     global filename
-    filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    filename = tkFileDialog.askopenfilename()  # show an "Open" dialog box and return the path to the selected file
     global outDir
-    outDir = askdirectory() + '/' + os.path.basename(filename).split('.')[0]
+    outDir = tkFileDialog.askdirectory() + '/' + os.path.basename(filename).split('.')[0]
     try:
         os.mkdir(outDir)
     except OSError as e:
@@ -67,44 +65,25 @@ if __name__ == "__main__":
         print('file not opened')
         sys.exit(1)
 
-    #frameRate = int(math.ceil(video.get(cv2.CAP_PROP_FPS)))
     numFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
     video.release()
+    numCores = multiprocessing.cpu_count()
+    framesChunks = []
+    chunkLength = numFrames / numCores
+    chunkStart = 0
+    chunkEnd = chunkStart + chunkLength
+    partialChunk = numFrames % numCores
 
+    for i in range(numCores):
+        framesChunks.append(multiprocessing.Process(target=getFrames, args=(filename, outDir, chunkStart, chunkEnd)))
+        chunkStart = chunkEnd
+        chunkEnd = chunkStart + chunkLength
 
-    p1 = multiprocessing.Process(target=getFrames, args=(filename, outDir, 0, numFrames/4))
-    p2 = multiprocessing.Process(target=getFrames, args=(filename, outDir, numFrames/4, numFrames/2))
-    p3 = multiprocessing.Process(target=getFrames, args=(filename, outDir, numFrames/2,(3*(numFrames/4))))
-    p4 = multiprocessing.Process(target=getFrames, args=(filename, outDir, (3*(numFrames/4)),numFrames))
+    if partialChunk > 0:
+        framesChunks.append(multiprocessing.Process(target=getFrames, args=(filename, outDir, chunkStart, numFrames)))
 
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
+    for i in range(len(framesChunks)):
+        framesChunks[i].start()
 
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-
-
-    #getFrames(0,numFrames/4)
-    #getFrames(numFrames/4,numFrames/2)
-    #getFrames(numFrames/2,(3*(numFrames/4)))
-    #getFrames((3*(numFrames/4)),numFrames)
-'''
-success, image = video.read()  # get the first frame of the video
-count = 0
-#frame = image
-while success:
-    success, image = video.read()    
-    if count % frameRate == 0:
-        outfilename = outDir + '/frame_' + str(count).zfill(6) + '.jpg'
-        cv2.imwrite(outfilename, image)
-    # exit if Escape is hit
-    if cv2.waitKey(10) == 27:
-        video.release()
-        break
-    count += 1
-
-'''
+    for i in range(len(framesChunks)):
+        framesChunks[i].join()
